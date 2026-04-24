751e41f7fd8377a2369acaa77a1d297667eaf3e8
Feyy35 <109589858+Feyy35@users.noreply.github.com>
2026-04-24 03:08:56 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-24T00:08:32.498Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-04-24T00:08:56.297Z
+Commit: Fix broken links in documentation
