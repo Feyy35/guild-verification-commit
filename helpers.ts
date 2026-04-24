55b32cd78092aa675ebf6dc1c6dadee97a8105ad
Feyy35 <109589858+Feyy35@users.noreply.github.com>
2026-04-24 03:08:09 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-04-24T00:07:48.052Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-04-24T00:08:09.198Z
+Commit: Resolve null pointer exception in user service
